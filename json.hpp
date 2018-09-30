ca1179c9420d07fc3b5fcb82815bf727de202a81
AlexDeveloper24 <alexchubukov12345@gmail.com>
2018-09-30 14:34:54 +0000

    std::optional

--- json.hpp
+++ json.hpp
@@ -1,184 +1,201 @@
 #ifndef JSON_H
-# define JSON_H
-# endif // ! JSON_H
-# include <iostream>
+#define JSON_H
+#endif  // ! JSON_H
+#include <iostream>
 #include <fstream>
 #include <cctype>
 #include <string>
 #include <any>
 #include <map>
 #include <vector>
+#include <optional>
 
-  struct Object {
-    std::map < std::string, std::any > field_obj;
-  };
+const int OPS_ERROR = 1;
+
+struct object_ {
+  std::map<std::string, std::any> field_object;
+};
 
 struct Array {
-  std::vector < std::any > field_arr;
+  std::vector<std::any> field_array;
 };
 
 class Json {
-  Object object;
-  Array vec;
+  object_ object_;
+  Array vector_;
 
-  public:
+ public:
+  Json();
 
-    Json();
+  Json(std::vector<std::any>&
+           value);  // Конструктор для присваивания массива полю Json
+                    // Конструктор из строки, содержащей Json-данные.
+  Json(const std::string& value);
 
-  Json(std::vector < std::any > & v); // Конструктор для присваивания массива полю Json
-  // Конструктор из строки, содержащей Json-данные.
-  Json(const std::string & s);
-
-  // Метод возвращает true, если данный экземпляр содержит в себе JSON-массив. Иначе false.
+  // Метод возвращает true, если данный экземпляр содержит в себе JSON-массив.
+  // Иначе false.
   bool is_array() const;
 
-  // Метод возвращает true, если данный экземпляр содержит в себе JSON-объект. Иначе false.
+  // Метод возвращает true, если данный экземпляр содержит в себе JSON-объект.
+  // Иначе false.
   bool is_object() const;
 
-  // Метод возвращает значение по ключу key, если экземпляр является JSON-объектом.
-  // Значение может иметь один из следующих типов: Json, std::string, double, bool или быть пустым.
+  // Метод возвращает значение по ключу key, если экземпляр является
+  // JSON-объектом.
+  // Значение может иметь один из следующих типов: Json, std::string, double,
+  // bool или быть пустым.
   // Если экземпляр является JSON-массивом, генерируется исключение.
-  std::any & operator[](const std::string & key);
+  std::optional<std::any> operator[](const std::string& key);
 
-  // Метод возвращает значение по индексу index, если экземпляр является JSON-массивом.
-  // Значение может иметь один из следующих типов: Json, std::string, double, bool или быть пустым.
+  // Метод возвращает значение по индексу index, если экземпляр является
+  // JSON-массивом.
+  // Значение может иметь один из следующих типов: Json, std::string, double,
+  // bool или быть пустым.
   // Если экземпляр является JSON-объектом, генерируется исключение.
-  std::any & operator[](unsigned int index);
+  std::optional<std::any> operator[](unsigned int index);
 
   // Метод возвращает объект класса Json из строки, содержащей Json-данные.
-  static Json parse(const std::string & s);
+  static Json parse(const std::string& s);
 
-  // Метод возвращает объекта класса Json из файла, содержащего Json-данные в текстовом формате.
-  static Json parseFile(const std::string & path_to_file);
+  // Метод возвращает объекта класса Json из файла, содержащего Json-данные в
+  // текстовом формате.
+  static Json parseFile(const std::string& path_to_file);
 };
 
 Json::Json() {}
 
-Json::Json(std::vector < std::any > & v) {
-  vec.field_arr = v;
-}
+Json::Json(std::vector<std::any>& value) { vector_.field_array = value; }
 
-Json::Json(const std::string & s) {
-  int i = 0;
-  std::string key = "", value1 = "";
+Json::Json(const std::string& s) {
+  int counter = 0;
+  std::string key = "", value_string = "";
   std::any value;
-  while (s[i] != '\0') {
-    if (s[i] == '"') {
-      ++i;
-      while (s[i] != '"') {
-        key += s[i];
-        ++i;
+  while (s[counter] != '\0') {
+    if (s[counter] == '"') {
+      ++counter;
+      while (s[counter] != '"') {
+        key += s[counter];
+        ++counter;
       }
     }
-    if (s[i] == ':') {
-      ++i;
-      while (s[i] == ' ') {
-        ++i;
+    if (s[counter] == ':') {
+      ++counter;
+      while (s[counter] == ' ') {
+        ++counter;
       }
-      if (s[i] == '"') {
-        ++i;
-        while (s[i] != '"') {
-          value1 += s[i];
-          ++i;
+      if (s[counter] == '"') {
+        ++counter;
+        while (s[counter] != '"') {
+          value_string += s[counter];
+          ++counter;
         }
-        object.field_obj.insert(std::pair < std::string, std::string > (key, value1));
-        key = value1 = "";
+        object_.field_object.insert(
+            std::pair<std::string, std::string>(key, value_string));
+        key = value_string = "";
       } else {
-        if (s[i] == '[') {
-          std::vector < std::any > v;
-          while (s[i] != ']') {
-            ++i;
-            while (s[i] == ' ') {
-              ++i;
+        if (s[counter] == '[') {
+          std::vector<std::any> vector_of_values;
+          while (s[counter] != ']') {
+            ++counter;
+            while (s[counter] == ' ') {
+              ++counter;
             }
-            if (isdigit(s[i])) {
-              v.push_back(atof( & (s[i])));
-              while (s[i] != ',' && s[i] != ']') {
-                ++i;
+            if (isdigit(s[counter])) {
+              vector_of_values.push_back(atof(&(s[counter])));
+              while (s[counter] != ',' && s[counter] != ']') {
+                ++counter;
               }
             } else {
-              if (s[i] == '"') {
-                ++i;
-                std::string s_arr = "";
-                while (s[i] != '"') {
-                  s_arr += s[i];
-                  ++i;
+              if (s[counter] == '"') {
+                ++counter;
+                std::string string_to_array = "";
+                while (s[counter] != '"') {
+                  string_to_array += s[counter];
+                  ++counter;
                 }
-                v.push_back(s_arr);
+                vector_of_values.push_back(string_to_array);
               } else {
-                if (s[i] == '[') {
-                  ++i;
-                  std::string s_mas = "";
-                  while (s[i] != ']') {
-                    s_mas += s[i];
-                    ++i;
+                if (s[counter] == '[') {
+                  ++counter;
+                  std::string string_to_parse = "";
+                  while (s[counter] != ']') {
+                    string_to_parse += s[counter];
+                    ++counter;
                   }
-                  s_mas += " ";
-                  ++i;
-                  v.push_back(Json(s_mas));
+                  string_to_parse += " ";
+                  ++counter;
+                  vector_of_values.push_back(Json(string_to_parse));
                 } else {
-                  if (s[i] == '{') {
-                    ++i;
-                    std::string str = "";
-                    while (s[i] != '}') {
-                      str += s[i];
-                      ++i;
+                  if (s[counter] == '{') {
+                    ++counter;
+                    std::string string_of_objects = "";
+                    while (s[counter] != '}') {
+                      string_of_objects += s[counter];
+                      ++counter;
                     }
-                    v.push_back(Json(str));
+                    vector_of_values.push_back(Json(string_of_objects));
                   }
                 }
               }
             }
           }
-          Json temp_arr(v);
+          Json temp_arr(vector_of_values);
           value = temp_arr;
-          object.field_obj.insert(std::pair < std::string, std::any > (key, value));
+          object_.field_object.insert(
+              std::pair<std::string, std::any>(key, value));
         } else {
-          if (s[i] == '{') {
-            std::string temp_s;
-            while (s[i] != '}') {
-              temp_s += s[i];
-              ++i;
+          if (s[counter] == '{') {
+            std::string string_objects;
+            while (s[counter] != '}') {
+              string_objects += s[counter];
+              ++counter;
             }
-            temp_s += '}';
-            Json temp(temp_s);
-            object.field_obj.insert(std::pair < std::string, Json > (key, temp));
+            string_objects += '}';
+            Json temp(string_objects);
+            object_.field_object.insert(
+                std::pair<std::string, Json>(key, temp));
           } else {
-            if (isdigit(s[i])) {
-              value = atof( & (s[i]));
-              object.field_obj.insert(std::pair < std::string, std::any > (key, value));
+            if (isdigit(s[counter])) {
+              value = atof(&(s[counter]));
+              object_.field_object.insert(
+                  std::pair<std::string, std::any>(key, value));
             } else {
-              if (s[i] == 'f') {
-                std::string s_bool = "";
-                while (s[i] != ',' && s[i] != '"' && s[i] != ' ') {
-                  s_bool += s[i];
-                  ++i;
+              if (s[counter] == 'f') {
+                std::string string_of_bool = "";
+                while (s[counter] != ',' && s[counter] != '"' &&
+                       s[counter] != ' ') {
+                  string_of_bool += s[counter];
+                  ++counter;
                 }
-                if (s_bool == "false") {
+                if (string_of_bool == "false") {
                   value = false;
-                  object.field_obj.insert(std::pair < std::string, std::any > (key, value));
+                  object_.field_object.insert(
+                      std::pair<std::string, std::any>(key, value));
                 }
               } else {
-                if (s[i] == 't') {
-                  std::string s_bool = "";
-                  while (s[i] != ',') {
-                    s_bool += s[i];
-                    ++i;
+                if (s[counter] == 't') {
+                  std::string string_of_bool = "";
+                  while (s[counter] != ',') {
+                    string_of_bool += s[counter];
+                    ++counter;
                   }
-                  if (s_bool == "true") {
+                  if (string_of_bool == "true") {
                     value = true;
-                    object.field_obj.insert(std::pair < std::string, std::any > (key, value));
+                    object_.field_object.insert(
+                        std::pair<std::string, std::any>(key, value));
                   }
                 } else {
-                  if (s[i] == 'n') {
-                    std::string s_bool = "";
-                    while (s[i] != ',' && s[i] != ' ' && s[i] != '"') {
-                      s_bool += s[i];
-                      ++i;
+                  if (s[counter] == 'n') {
+                    std::string string_of_null = "";
+                    while (s[counter] != ',' && s[counter] != ' ' &&
+                           s[counter] != '"') {
+                      string_of_null += s[counter];
+                      ++counter;
                     }
-                    if (s_bool == "null") {
+                    if (string_of_null == "null") {
                       value = NULL;
-                      object.field_obj.insert(std::pair < std::string, std::any > (key, value));
+                      object_.field_object.insert(
+                          std::pair<std::string, std::any>(key, value));
                     }
                   }
                 }
@@ -189,43 +206,40 @@ Json::Json(const std::string & s) {
         key = "";
       }
     }
-    ++i;
+    ++counter;
   }
 }
 
-bool Json::is_array() const {
-  return !vec.field_arr.empty();
-}
+bool Json::is_array() const { return !vector_.field_array.empty(); }
 
-bool Json::is_object() const {
-  return !object.field_obj.empty();
-}
+bool Json::is_object() const { return !object_.field_object.empty(); }
 
-std::any & Json::operator[](const std::string & key) {
+std::optional<std::any> Json::operator[](const std::string& key) {
   if (this->is_object()) {
-    for (auto it = object.field_obj.begin(); it != object.field_obj.end(); ++it) {
+    for (auto it = object_.field_object.begin();
+         it != object_.field_object.end(); ++it) {
       if (it->first == key) {
         return it->second;
       }
     }
+    return std::nullopt;
   }
-  throw 1;
+  throw OPS_ERROR;
 }
 
-std::any & Json::operator[](unsigned int index) {
+std::optional<std::any> Json::operator[](unsigned int index) {
   if (this->is_array()) {
-    if (index < vec.field_arr.size()) {
-      return this->vec.field_arr[index];
+    if (index < vector_.field_array.size()) {
+      return this->vector_.field_array[index];
     }
+    return std::nullopt;
   }
-  throw 1;
+  throw OPS_ERROR;
 }
 
-Json Json::parse(const std::string & s) {
-  return Json(s);
-}
+Json Json::parse(const std::string& s) { return Json(s); }
 
-Json Json::parseFile(const std::string & path_to_file) {
+Json Json::parseFile(const std::string& path_to_file) {
   std::ifstream fin(path_to_file);
   char temp;
   std::string s;
